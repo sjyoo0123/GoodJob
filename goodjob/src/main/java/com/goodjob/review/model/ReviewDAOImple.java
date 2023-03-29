f6e922b19d845368d97b51b338798a175d7c78d1
김선훈\kshlu <seonoon@naver.com>
2023-03-29 17:47:19 +0900

    review 시작 폴더만 만듬

---
+++ goodjob/src/main/java/com/goodjob/review/model/ReviewDAOImple.java
@@ -0,0 +1,20 @@
+package com.goodjob.review.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class ReviewDAOImple implements ReviewDAO {
+	
+	@Override
+	public List<ReviewDTO> reviewList(Map map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int getTotalCnt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
