d4470d4e21159f7fc4f306295f447ea084516dd6
AEGU\mchin <mchina94@gmail.com>
2023-04-13 16:50:32 +0900

    2023-04-13 인재리스트 완료

--- goodjob/src/main/java/com/goodjob/injaelist/model/InjaeListDAO.java
+++ goodjob/src/main/java/com/goodjob/injaelist/model/InjaeListDAO.java
@@ -1,5 +1,10 @@
 package com.goodjob.injaelist.model;
 
+import java.util.List;
+import java.util.Map;
+
 public interface InjaeListDAO {
 
+	public List<InjaeListDTO> injaeListGet(Map map);
+	public int injaeListTotalCnt(Map map);
 }
--- goodjob/src/main/java/com/goodjob/injaelist/model/InjaeListDAOImple.java
+++ goodjob/src/main/java/com/goodjob/injaelist/model/InjaeListDAOImple.java
@@ -1,5 +1,30 @@
 package com.goodjob.injaelist.model;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
 public class InjaeListDAOImple implements InjaeListDAO {
 
+	SqlSession sqlMap;
+	public InjaeListDAOImple() {
+		// TODO Auto-generated constructor stub
+	}
+	public SqlSession getSqlMap() {
+		return sqlMap;
+	}
+	public void setSqlMap(SqlSession sqlMap) {
+		this.sqlMap = sqlMap;
+	}
+	@Override
+	public List<InjaeListDTO> injaeListGet(Map map) {
+		List<InjaeListDTO> lists=sqlMap.selectList("injaeListGet", map);
+		return lists;
+	}
+	@Override
+	public int injaeListTotalCnt(Map map) {
+		int total=sqlMap.selectOne("injaeListTotalCnt", map);
+		return total;
+	}
 }
